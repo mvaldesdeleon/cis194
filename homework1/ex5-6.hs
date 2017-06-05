56d46a76f8f84022a686a61a9e6ba33c40f91ec1
Martín Valdés de León <m.valdesdeleon@sovanta.com>
2017-06-06 00:52:28 +0200

    Adds `hanoiMoves` function.

--- homework1/ex5-6.hs
+++ homework1/ex5-6.hs
@@ -6,3 +6,6 @@ type Move = (Peg, Peg)
 hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
 hanoi 0 _ _ _ = []
 hanoi n a b c = hanoi (n-1) a c b ++ [(a, b)] ++ hanoi (n - 1) c b a
+
+hanoiMoves :: Integer -> Integer
+hanoiMoves n = (fromIntegral . length) (hanoi n "a" "b" "c")
