7200831c67bcc2685d001b16bad19af9eeffea30
Martín Valdés de León <m.valdesdeleon@sovanta.com>
2017-08-05 21:49:49 +0200

    Week 5, exercise 3.

--- week5/Calc.hs
+++ week5/Calc.hs
@@ -1,6 +1,19 @@
 import ExprT
 import Parser
 
+class Expr e where
+    lit :: Integer -> e
+    add :: e -> e -> e
+    mul :: e -> e -> e
+
+instance Expr ExprT where
+    lit = Lit
+    add = Add
+    mul = Mul
+
+reify :: ExprT -> ExprT
+reify = id
+
 eval :: ExprT -> Integer
 eval expr = case expr of
     Lit n   -> n
