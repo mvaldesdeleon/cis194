353d1e6ffe33cf64df55204203a9a9548ba425f4
Martín Valdés de León <m.valdesdeleon@sovanta.com>
2017-08-05 21:21:10 +0200

    Week 5, exercise 2.

--- week5/Calc.hs
+++ week5/Calc.hs
@@ -1,7 +1,11 @@
 import ExprT
+import Parser
 
 eval :: ExprT -> Integer
 eval expr = case expr of
     Lit n   -> n
     Add a b -> eval a + eval b
     Mul a b -> eval a * eval b
+
+evalStr :: String -> Maybe Integer
+evalStr = fmap eval . parseExp Lit Add Mul
