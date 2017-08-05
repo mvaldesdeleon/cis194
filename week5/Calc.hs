0a055f99a13096132bcf80cc4b7439ab4ed8d3fb
Martín Valdés de León <m.valdesdeleon@sovanta.com>
2017-08-05 23:09:38 +0200

    Week 5, exercise 4.

--- week5/Calc.hs
+++ week5/Calc.hs
@@ -14,6 +14,29 @@ instance Expr ExprT where
 reify :: ExprT -> ExprT
 reify = id
 
+instance Expr Integer where
+    lit = id
+    add = (+)
+    mul = (*)
+
+instance Expr Bool where
+    lit = (<=0)
+    add = (||)
+    mul = (&&)
+
+newtype MinMax = MinMax Integer deriving (Eq, Show)
+newtype Mod7   = Mod7 Integer deriving (Eq, Show)
+
+instance Expr MinMax where
+    lit = MinMax
+    add (MinMax a) (MinMax b) = lit $ max a b
+    mul (MinMax a) (MinMax b)= lit $ min a b
+
+instance Expr Mod7 where
+    lit a = Mod7 $ a `mod` 7
+    add (Mod7 a) (Mod7 b) = lit $ (a + b)
+    mul (Mod7 a) (Mod7 b) = lit $ (a * b)
+
 eval :: ExprT -> Integer
 eval expr = case expr of
     Lit n   -> n
@@ -22,3 +45,11 @@ eval expr = case expr of
 
 evalStr :: String -> Maybe Integer
 evalStr = fmap eval . parseExp Lit Add Mul
+
+testExp :: Expr a => Maybe a
+testExp = parseExp lit add mul "(3 * -4) + 5"
+
+testInteger = testExp :: Maybe Integer
+testBool = testExp :: Maybe Bool
+testMM = testExp :: Maybe MinMax
+testSat = testExp :: Maybe Mod7
