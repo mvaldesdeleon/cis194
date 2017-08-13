9cfeec184cc37280b018b53c8f8817128f2ee6ba
Martín Valdés de León <m.valdesdeleon@sovanta.com>
2017-08-13 14:45:15 +0200

    Week 6, exercise 4.

--- week6/Fibonacci.hs
+++ week6/Fibonacci.hs
@@ -16,3 +16,12 @@ streamToList (Cons a as) = [a] ++ streamToList as
 
 instance Show a => Show (Stream a) where
     show = show . take 20 . streamToList
+
+streamRepeat :: a -> Stream a
+streamRepeat x = Cons x (streamRepeat x)
+
+streamMap :: (a -> b) -> Stream a -> Stream b
+streamMap fn (Cons a as) = Cons (fn a) $ streamMap fn as
+
+streamFromSeed :: (a -> a) -> a -> Stream a
+streamFromSeed fn x = Cons x $ streamFromSeed fn (fn x)
