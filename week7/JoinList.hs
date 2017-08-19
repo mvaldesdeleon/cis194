01e71476a139974a7da430c7832781a926847a94
Martín Valdés de León <m.valdesdeleon@sovanta.com>
2017-08-19 10:45:50 +0200

    Week 7, exercise 2, second part.

--- week7/JoinList.hs
+++ week7/JoinList.hs
@@ -43,7 +43,11 @@ indexJ i l | Size i > tagSize l - 1          = Nothing
 indexJ 0 (Single _ a)                        = Just a
 indexJ i (Append _ l r) | Size i < tagSize l = indexJ i l
                         | otherwise          = indexJ (i - (getSize . tagSize $ l)) r
---
--- dropJ :: (Sized b, Monoid b) => Int -> JoinList b a -> JoinList b a
+
+dropJ :: (Sized b, Monoid b) => Int -> JoinList b a -> JoinList b a
+dropJ i l | i <= 0                              = l
+dropJ i l | Size i > tagSize l - 1              = Empty
+dropJ i (Append m l r) | Size i - 1 < tagSize l = (dropJ i l) +++ r
+                       | otherwise              = dropJ (i - (getSize . tagSize $ l)) r
 
 -- takeJ :: (Sized b, Monoid b) => Int -> JoinList b a -> JoinList b a
