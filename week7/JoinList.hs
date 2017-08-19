6527b3e4578b15f840e372714a27cc93d1d472dc
Martín Valdés de León <m.valdesdeleon@sovanta.com>
2017-08-19 10:47:55 +0200

    Small refactor.

--- week7/JoinList.hs
+++ week7/JoinList.hs
@@ -34,20 +34,20 @@ y = Single (Size 1) "boop"
 z :: JoinList Size String
 z = Append (Size 3) (Append (Size 2) (Single (Size 1) "best") (Single (Size 1) "juice")) (Single (Size 1) "tomato")
 
-tagSize :: (Sized b, Monoid b) => JoinList b a -> Size
-tagSize = size . tag
+tagSize :: (Sized b, Monoid b) => JoinList b a -> Int
+tagSize = getSize . size . tag
 
 indexJ :: (Sized b, Monoid b) => Int -> JoinList b a -> Maybe a
 indexJ i _ | i < 0                           = Nothing
-indexJ i l | Size i > tagSize l - 1          = Nothing
+indexJ i l | i > tagSize l - 1          = Nothing
 indexJ 0 (Single _ a)                        = Just a
-indexJ i (Append _ l r) | Size i < tagSize l = indexJ i l
-                        | otherwise          = indexJ (i - (getSize . tagSize $ l)) r
+indexJ i (Append _ l r) | i < tagSize l = indexJ i l
+                        | otherwise          = indexJ (i - tagSize l) r
 
 dropJ :: (Sized b, Monoid b) => Int -> JoinList b a -> JoinList b a
 dropJ i l | i <= 0                              = l
-dropJ i l | Size i > tagSize l - 1              = Empty
-dropJ i (Append m l r) | Size i - 1 < tagSize l = (dropJ i l) +++ r
-                       | otherwise              = dropJ (i - (getSize . tagSize $ l)) r
+dropJ i l | i > tagSize l - 1              = Empty
+dropJ i (Append m l r) | i - 1 < tagSize l = (dropJ i l) +++ r
+                       | otherwise              = dropJ (i - tagSize l) r
 
 -- takeJ :: (Sized b, Monoid b) => Int -> JoinList b a -> JoinList b a
