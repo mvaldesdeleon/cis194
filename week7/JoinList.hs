05fc5868c3fcc0882352fb4ee18d20ffc6f33b87
Martín Valdés de León <m.valdesdeleon@sovanta.com>
2017-08-19 10:48:47 +0200

    Style, my god...

--- week7/JoinList.hs
+++ week7/JoinList.hs
@@ -38,16 +38,16 @@ tagSize :: (Sized b, Monoid b) => JoinList b a -> Int
 tagSize = getSize . size . tag
 
 indexJ :: (Sized b, Monoid b) => Int -> JoinList b a -> Maybe a
-indexJ i _ | i < 0                           = Nothing
+indexJ i _ | i < 0                      = Nothing
 indexJ i l | i > tagSize l - 1          = Nothing
-indexJ 0 (Single _ a)                        = Just a
+indexJ 0 (Single _ a)                   = Just a
 indexJ i (Append _ l r) | i < tagSize l = indexJ i l
-                        | otherwise          = indexJ (i - tagSize l) r
+                        | otherwise     = indexJ (i - tagSize l) r
 
 dropJ :: (Sized b, Monoid b) => Int -> JoinList b a -> JoinList b a
-dropJ i l | i <= 0                              = l
+dropJ i l | i <= 0                         = l
 dropJ i l | i > tagSize l - 1              = Empty
 dropJ i (Append m l r) | i - 1 < tagSize l = (dropJ i l) +++ r
-                       | otherwise              = dropJ (i - tagSize l) r
+                       | otherwise         = dropJ (i - tagSize l) r
 
 -- takeJ :: (Sized b, Monoid b) => Int -> JoinList b a -> JoinList b a
