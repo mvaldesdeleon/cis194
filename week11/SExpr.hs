0b9fba56e96eb026b5be3fe7521eb3365262e2d0
Martín Valdés de León <m.valdesdeleon@sovanta.com>
2017-08-30 00:02:14 +0200

    Week 11, exercise 1.

--- week11/SExpr.hs
+++ week11/SExpr.hs
@@ -12,10 +12,10 @@ import Control.Applicative
 ------------------------------------------------------------
 
 zeroOrMore :: Parser a -> Parser [a]
-zeroOrMore p = undefined
+zeroOrMore p = oneOrMore p <|> pure []
 
 oneOrMore :: Parser a -> Parser [a]
-oneOrMore p = undefined
+oneOrMore p = (:) <$> p <*> zeroOrMore p
 
 ------------------------------------------------------------
 --  2. Utilities
