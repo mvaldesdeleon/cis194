963227321c7644594a27b32c5d7c428e377ee0df
Martín Valdés de León <m.valdesdeleon@sovanta.com>
2017-09-01 11:55:34 +0200

    Week 12, exercise 2.

--- week12/Risk.hs
+++ week12/Risk.hs
@@ -2,6 +2,8 @@
 
 module Risk where
 
+import Data.List
+import Control.Applicative
 import Control.Monad.Random
 
 ------------------------------------------------------------
@@ -26,3 +28,29 @@ die = getRandom
 type Army = Int
 
 data Battlefield = Battlefield { attackers :: Army, defenders :: Army }
+    deriving Show
+
+effective :: Battlefield -> Battlefield
+effective b = Battlefield (min 3 . subtract 1 . attackers $ b) (min 2 . defenders $ b)
+
+data BattlefieldDice = BattlefieldDice { attackerDice :: [DieValue], defenderDice :: [DieValue] }
+
+roll :: Battlefield -> Rand StdGen (Battlefield, BattlefieldDice)
+roll b = replicateM (attackers eb) die >>= \dva ->
+         replicateM (defenders eb) die >>= \dvd ->
+         return (b, BattlefieldDice dva dvd)
+         where
+            eb = effective b
+
+resolve :: (Battlefield, BattlefieldDice) -> Battlefield
+resolve (b, bd) = Battlefield a d
+    where
+        pairs = liftA2 zip (reverse . sort . attackerDice) (reverse . sort . defenderDice) bd
+        win (a, d) = a > d
+        a = attackers b - (length . filter win $ pairs)
+        d = defenders b - (length . filter (not . win) $ pairs)
+
+battle :: Battlefield -> Rand StdGen Battlefield
+battle = mapRand resolveG . roll
+    where
+        resolveG (a, g) = (resolve a, g)
